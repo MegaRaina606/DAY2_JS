6cc57209bea7d7bb8e3a9dc28a3403ea205dcfa9
MegaRaina606 <84876072+MegaRaina606@users.noreply.github.com>
2021-07-07 16:40:15 +0530

    Add files via upload
    DAY2_JS

---
+++ DAY2_JS.js
@@ -0,0 +1,91 @@
+//a=10
+//b=5
+// add = a+b 
+// console.log(add)
+
+//sub=a-b
+//console.log(sub)
+
+//mul = a*b 
+//console.log(mul)
+
+//div = a/b 
+//console.log(div)
+
+//rem = a%b 
+//console.log(rem)
+
+//Relational Operations
+
+//> >= < <= == !=
+
+//Boolean
+//c=a<b //false
+//console.log(c)
+
+//c= a==b //false
+//console.log(c)
+
+//if else 
+
+/*
+if(a>b){
+  console.log("Greater")
+}
+else if(a<b){
+  console.log("Smaller")
+}
+else{
+  console.log("Equal")
+}
+*/
+/*
+2000 -> L divible by 400
+2024 -> L
+2100 -> L 
+*/
+/*
+a=2100
+if(a%4 == 0){
+   if(a%100 == 0){
+     if(a%400==0){
+       console.log("Leap Year")
+     }
+     else{
+       console.log("Its not a Leap Year")
+     }
+   }
+   else{
+     console.log("Leap Year")
+     
+   }
+}
+else{
+  console.log("Leap Year")
+}
+*/
+/*
+for(i=1;i<=5;i++)
+{
+  console.log(i)
+}
+console.log(i)
+*/
+
+/*
+for(i=1;i<=5;i++){
+  console.log("*")
+}
+console.log(i)
+*/
+
+//Assignment Question
+
+for(i=1;i<2;i++){
+  console.log("*****")
+}
+
+
+
+
+  
